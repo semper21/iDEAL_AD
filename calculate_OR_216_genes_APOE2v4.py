0c74ac15cdcb5236cd09d4b13cddc61ab44fbbbe
ywkim <ywkim@bcm.edu>
2019-10-07 15:21:45 -0500

    -Changed the packaged used to calculate OR in order to get confidence interval as well! -Checked that OR does not differ -P-value differs only by little (E-6) -Note: CI should not include 1 even if p-value < 0.05

--- calculate_OR_216_genes_APOE2v4.py
+++ calculate_OR_216_genes_APOE2v4.py
@@ -2,14 +2,20 @@
 Created on 
 
 @author: ywkim
+
+10/07/19
+-Changed the packaged used to calculate OR in order to get confidence interval as well!
+-Checked that OR does not differ!
+-P-value differs only by little (E-6)
+-Note: CI should not include 1 even if p-value < 0.05
 '''
 import csv
 import numpy as np
 import pandas as pd
-import scipy.stats as stats
 
+import statsmodels.api as sm
 
-def calculate_or(a, b, c, d):
+def calculate_or_with_ci(a, b, c, d):
     table = np.zeros((2, 2))
     # a: affected with mutation
     table[0][0] = a
@@ -19,11 +25,14 @@ def calculate_or(a, b, c, d):
     table[1][0] = c
     # d: healthy w/o mutation
     table[1][1] = d
-    try:
-        oddsratio, pvalue = stats.fisher_exact(table)
-    except ValueError:
-        embed()
-    return oddsratio, pvalue
+
+    or_table = sm.stats.Table2x2(table)
+
+    odds_ratio = or_table.oddsratio
+    p_value = or_table.oddsratio_pvalue()
+    confidence_interval = list(or_table.oddsratio_confint())
+
+    return odds_ratio, p_value, confidence_interval
 
 
 if __name__ == '__main__':
@@ -38,13 +47,12 @@ if __name__ == '__main__':
     control_file = dirc + '216_updated/216_mutation_count_APOE4-HC.csv'
 
     df_case = pd.read_csv(case_file, sep=',')
-    df_case = df_case[['Gene', 'Sub', 'Action', 'Count', 'Het:Hom']]
     df_control = pd.read_csv(control_file, sep=',')
-    df_control = df_control[['Gene', 'Sub', 'Action', 'Count', 'Het:Hom']]
 
     df = df_case.merge(df_control, on=['Gene', 'Sub', 'Action'], how='outer')
     df = df.fillna(0)
-    df.columns = ['Gene', 'Sub', 'Action', '# APOE2-AD', 'Het:Hom APOE2-AD', '# APOE4-HC', 'Het:Hom APOE4-HC']
+    df.columns = ['Gene', 'Sub', 'Action', '# APOE2-AD', 'Het APOE2-AD', 'Hom APOE2-AD', 'Het:Hom APOE2-AD',
+                  '# APOE4-HC','Het APOE4-HC', 'Hom APOE4-HC',  'Het:Hom APOE4-HC']
     df.to_csv(dirc + '216_all_mutation_count_APOE3_withEA.csv', sep=',', index=False)
     df.to_csv(dirc + '216_updated/216_all_mutation_count_APOE2v4_withEA.csv', sep=',', index=False)
 
@@ -52,10 +60,11 @@ if __name__ == '__main__':
     ad_all = 179
 
     # only 69 genes
-    outputFile = dirc + '216_updated/69hits_all_mutation_count_APOE2v4_withEA_OR.csv'
+    outputFile = dirc + '216_updated/69hits_all_mutation_count_APOE2v4_withEA_OR_CI.csv'
     with open(outputFile, 'w') as f:
         writer = csv.writer(f, delimiter=',')
-        writer.writerow(['Gene', 'Sub', 'Action', 'Het:Hom Case', 'Het:Hom Control', '# Case', '# Control', 'OR', 'p-value'])
+        writer.writerow(['Gene', 'Sub', 'Action', 'Het:Hom Case', 'Het:Hom Control', 'Het_OR', 'Het_CI', 'Het_p-value',
+                         'Hom_OR', 'Hom_CI', 'Hom_p-value', '# Case', '# Control', 'OR', 'CI', 'p-value'])
 
         for gene in genes:
             sub_info = df[df['Gene'] == gene].values
@@ -63,9 +72,13 @@ if __name__ == '__main__':
                 sub = row[1]
                 action = row[2]
                 ad = row[3]
-                ratio_ad = row[4]
-                hc = row[5]
-                ratio_hc = row[6]
+                het_ad = row[4]
+                hom_ad = row[5]
+                ratio_ad = row[6]
+                hc = row[7]
+                het_hc = row[8]
+                hom_hc = row[9]
+                ratio_hc = row[10]
                 diff = int(ad) - int(hc)
 
                 if action in ['silent', 'no_action']:
@@ -73,9 +86,28 @@ if __name__ == '__main__':
 
                 else:
                     if ad == 0 or hc == 0:
-                        oddsratio, pvalue = calculate_or(ad + 1, hc + 1, ad_all - ad + 1, hc_all - hc + 1)
+                        odds_ratio, p_value, ci = calculate_or_with_ci(ad + 1, hc + 1, ad_all - ad + 1, hc_all - hc + 1)
                     else:
-                        oddsratio, pvalue = calculate_or(ad, hc, ad_all - ad, hc_all - hc)
+                        odds_ratio, p_value, ci = calculate_or_with_ci(ad, hc, ad_all - ad, hc_all - hc)
 
-                    info = [gene, sub, action, ratio_ad, ratio_hc, ad, hc, oddsratio, pvalue]
+                    if het_ad != 0 and het_hc != 0:
+                        het_or, het_p, het_ci = calculate_or_with_ci(het_ad, het_hc, ad_all - het_ad, hc_all - het_hc)
+                    else:
+                        het_or = 'NA'
+                        het_ci = 'NA'
+                        het_p = 'NA'
+
+                    if hom_ad != 0 and hom_hc != 0:
+                        hom_or, hom_p, hom_ci = calculate_or_with_ci(hom_ad, hom_hc, ad_all - hom_ad, hc_all - hom_hc)
+                    else:
+                        hom_or = 'NA'
+                        hom_ci = 'NA'
+                        hom_p = 'NA'
+
+                    if ratio_ad == 0:
+                        ratio_ad = '0:0'
+                    if ratio_hc == 0:
+                        ratio_hc = '0:0'
+                    info = [gene, sub, action, ratio_ad, ratio_hc, het_or, het_ci, het_p, hom_or, hom_ci, hom_p,
+                            ad, hc, odds_ratio, ci, p_value]
                     writer.writerow(info)
